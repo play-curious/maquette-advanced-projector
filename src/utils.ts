8ef4813f982de513673af016adaa3a26b89b6ed7
Johan Gorter <johan-gorter@users.noreply.github.com>
2021-07-09 15:48:02 +0200

    fixall with newest typescript-assistant

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { ProjectionOptions } from 'maquette';
+import { ProjectionOptions } from "maquette";
 
 const DEFAULT_PROJECTION_OPTIONS: ProjectionOptions = {
   namespace: undefined,
@@ -7,9 +7,11 @@ const DEFAULT_PROJECTION_OPTIONS: ProjectionOptions = {
   styleApplyer: (domNode: HTMLElement, styleName: string, value: string) => {
     // Provides a hook to add vendor prefixes for browsers that still need it.
     (domNode.style as any)[styleName] = value;
-  }
+  },
 };
 
-export let applyDefaultProjectionOptions = (projectorOptions?: ProjectionOptions) => {
+export let applyDefaultProjectionOptions = (
+  projectorOptions?: ProjectionOptions
+): ProjectionOptions => {
   return { ...DEFAULT_PROJECTION_OPTIONS, ...projectorOptions };
 };
